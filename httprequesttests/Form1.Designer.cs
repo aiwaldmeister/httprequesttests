aea00e489be970de283ab8d5fbe8258df3b0efac
aiwaldmeister <schmidmichael1985@googlemail.com>
2019-11-28 17:41:39 +0100

    Umgestellt auf Liste mit Items der Klasse patterndata

--- httprequesttests/Form1.Designer.cs
+++ httprequesttests/Form1.Designer.cs
@@ -28,87 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox_response = new System.Windows.Forms.TextBox();
-            this.textBox_url = new System.Windows.Forms.TextBox();
-            this.button_request = new System.Windows.Forms.Button();
-            this.textBox_LoginURL = new System.Windows.Forms.TextBox();
-            this.button_Login = new System.Windows.Forms.Button();
-            this.button_WebClient_Login = new System.Windows.Forms.Button();
             this.button_Webclient_Request = new System.Windows.Forms.Button();
-            this.textBox_BaseURL = new System.Windows.Forms.TextBox();
             this.DG1 = new System.Windows.Forms.DataGridView();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button_Timer_Toggle = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.views = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.sells = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.wishlists = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ratings = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Durchschnitt = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.DG1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox_response
             // 
-            this.textBox_response.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox_response.Location = new System.Drawing.Point(2, 32);
+            this.textBox_response.Location = new System.Drawing.Point(599, 36);
             this.textBox_response.Multiline = true;
             this.textBox_response.Name = "textBox_response";
             this.textBox_response.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox_response.Size = new System.Drawing.Size(1114, 287);
+            this.textBox_response.Size = new System.Drawing.Size(465, 634);
             this.textBox_response.TabIndex = 1;
-            // 
-            // textBox_url
-            // 
-            this.textBox_url.Location = new System.Drawing.Point(307, 8);
-            this.textBox_url.Name = "textBox_url";
-            this.textBox_url.Size = new System.Drawing.Size(218, 20);
-            this.textBox_url.TabIndex = 2;
-            this.textBox_url.Text = "https://www.crazypatterns.net/de/patman";
-            this.textBox_url.Visible = false;
-            // 
-            // button_request
-            // 
-            this.button_request.Location = new System.Drawing.Point(531, 6);
-            this.button_request.Name = "button_request";
-            this.button_request.Size = new System.Drawing.Size(75, 23);
-            this.button_request.TabIndex = 0;
-            this.button_request.Text = "Request";
-            this.button_request.UseVisualStyleBackColor = true;
-            this.button_request.Click += new System.EventHandler(this.button_request_Click);
-            // 
-            // textBox_LoginURL
-            // 
-            this.textBox_LoginURL.Location = new System.Drawing.Point(83, 7);
-            this.textBox_LoginURL.Name = "textBox_LoginURL";
-            this.textBox_LoginURL.Size = new System.Drawing.Size(218, 20);
-            this.textBox_LoginURL.TabIndex = 2;
-            this.textBox_LoginURL.Text = "https://www.crazypatterns.net/de/users/login";
-            this.textBox_LoginURL.Visible = false;
-            // 
-            // button_Login
-            // 
-            this.button_Login.Location = new System.Drawing.Point(2, 5);
-            this.button_Login.Name = "button_Login";
-            this.button_Login.Size = new System.Drawing.Size(75, 23);
-            this.button_Login.TabIndex = 0;
-            this.button_Login.Text = "Login";
-            this.button_Login.UseVisualStyleBackColor = true;
-            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
-            // 
-            // button_WebClient_Login
-            // 
-            this.button_WebClient_Login.Location = new System.Drawing.Point(642, 6);
-            this.button_WebClient_Login.Name = "button_WebClient_Login";
-            this.button_WebClient_Login.Size = new System.Drawing.Size(110, 23);
-            this.button_WebClient_Login.TabIndex = 3;
-            this.button_WebClient_Login.Text = "ConnectWebClient";
-            this.button_WebClient_Login.UseVisualStyleBackColor = true;
-            this.button_WebClient_Login.Click += new System.EventHandler(this.button_WebClient_Login_Click);
             // 
             // button_Webclient_Request
             // 
-            this.button_Webclient_Request.Location = new System.Drawing.Point(758, 6);
+            this.button_Webclient_Request.Location = new System.Drawing.Point(12, 7);
             this.button_Webclient_Request.Name = "button_Webclient_Request";
             this.button_Webclient_Request.Size = new System.Drawing.Size(110, 23);
             this.button_Webclient_Request.TabIndex = 3;
@@ -116,74 +63,93 @@
             this.button_Webclient_Request.UseVisualStyleBackColor = true;
             this.button_Webclient_Request.Click += new System.EventHandler(this.button_Webclient_Request_Click);
             // 
-            // textBox_BaseURL
-            // 
-            this.textBox_BaseURL.Location = new System.Drawing.Point(874, 7);
-            this.textBox_BaseURL.Name = "textBox_BaseURL";
-            this.textBox_BaseURL.Size = new System.Drawing.Size(218, 20);
-            this.textBox_BaseURL.TabIndex = 2;
-            this.textBox_BaseURL.Text = "https://www.crazypatterns.net/de/";
-            this.textBox_BaseURL.Visible = false;
-            // 
             // DG1
             // 
+            this.DG1.AllowUserToAddRows = false;
+            this.DG1.AllowUserToDeleteRows = false;
             this.DG1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DG1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.name,
             this.number,
+            this.name,
             this.views,
             this.sells,
             this.wishlists,
-            this.ratings});
-            this.DG1.Location = new System.Drawing.Point(12, 361);
+            this.ratings,
+            this.Durchschnitt});
+            this.DG1.Location = new System.Drawing.Point(12, 36);
             this.DG1.Name = "DG1";
-            this.DG1.Size = new System.Drawing.Size(1104, 309);
+            this.DG1.ReadOnly = true;
+            this.DG1.RowHeadersVisible = false;
+            this.DG1.Size = new System.Drawing.Size(570, 634);
             this.DG1.TabIndex = 4;
             // 
-            // name
+            // button_Timer_Toggle
             // 
-            this.name.HeaderText = "Name";
-            this.name.Name = "name";
+            this.button_Timer_Toggle.Location = new System.Drawing.Point(128, 7);
+            this.button_Timer_Toggle.Name = "button_Timer_Toggle";
+            this.button_Timer_Toggle.Size = new System.Drawing.Size(75, 23);
+            this.button_Timer_Toggle.TabIndex = 5;
+            this.button_Timer_Toggle.Text = "Activate Tracking";
+            this.button_Timer_Toggle.UseVisualStyleBackColor = true;
             // 
             // number
             // 
             this.number.HeaderText = "#";
             this.number.Name = "number";
+            this.number.ReadOnly = true;
+            this.number.Width = 50;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 300;
             // 
             // views
             // 
             this.views.HeaderText = "Views";
             this.views.Name = "views";
+            this.views.ReadOnly = true;
+            this.views.Width = 40;
             // 
             // sells
             // 
             this.sells.HeaderText = "Verk.";
             this.sells.Name = "sells";
+            this.sells.ReadOnly = true;
+            this.sells.Width = 40;
             // 
             // wishlists
             // 
             this.wishlists.HeaderText = "WL";
             this.wishlists.Name = "wishlists";
+            this.wishlists.ReadOnly = true;
+            this.wishlists.Width = 40;
             // 
             // ratings
             // 
             this.ratings.HeaderText = "Bew.";
             this.ratings.Name = "ratings";
+            this.ratings.ReadOnly = true;
+            this.ratings.Width = 40;
+            // 
+            // Durchschnitt
+            // 
+            this.Durchschnitt.HeaderText = "DS";
+            this.Durchschnitt.Name = "Durchschnitt";
+            this.Durchschnitt.ReadOnly = true;
+            this.Durchschnitt.Width = 40;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1128, 682);
+            this.ClientSize = new System.Drawing.Size(594, 682);
+            this.Controls.Add(this.button_Timer_Toggle);
             this.Controls.Add(this.DG1);
             this.Controls.Add(this.button_Webclient_Request);
-            this.Controls.Add(this.button_WebClient_Login);
             this.Controls.Add(this.textBox_response);
-            this.Controls.Add(this.textBox_BaseURL);
-            this.Controls.Add(this.textBox_LoginURL);
-            this.Controls.Add(this.textBox_url);
-            this.Controls.Add(this.button_Login);
-            this.Controls.Add(this.button_request);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -196,20 +162,17 @@
 
         #endregion
         private System.Windows.Forms.TextBox textBox_response;
-        private System.Windows.Forms.TextBox textBox_url;
-        private System.Windows.Forms.Button button_request;
-        private System.Windows.Forms.TextBox textBox_LoginURL;
-        private System.Windows.Forms.Button button_Login;
-        private System.Windows.Forms.Button button_WebClient_Login;
         private System.Windows.Forms.Button button_Webclient_Request;
-        private System.Windows.Forms.TextBox textBox_BaseURL;
         private System.Windows.Forms.DataGridView DG1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.Button button_Timer_Toggle;
+        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
         private System.Windows.Forms.DataGridViewTextBoxColumn views;
         private System.Windows.Forms.DataGridViewTextBoxColumn sells;
         private System.Windows.Forms.DataGridViewTextBoxColumn wishlists;
         private System.Windows.Forms.DataGridViewTextBoxColumn ratings;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Durchschnitt;
     }
 }
 
--- httprequesttests/Form1.cs
+++ httprequesttests/Form1.cs
@@ -12,14 +12,53 @@ namespace httprequesttests
 {
     public partial class Form1 : Form
     {
-        string formUrl;
-        string myURL;
         string myUsername;
         string myPW;
-        string cookieHeader;
-        string myCookie;
-        string myBaseURL;
+        string myBaseURL = "https://www.crazypatterns.net/de/";
+        string myLoginPage = "users/login";
+        string myPatmanPage = "patman";
         CookieAwareWebClient client = new CookieAwareWebClient();
+        List<patterndata> referenceList = new List<patterndata>();
+        List<patterndata> newList = new List<patterndata>();
+
+        public class patterndata
+        {
+            public string number;
+            public string name;
+            public string views;
+            public string sells;
+            public string wishlists;
+            public string ratings;
+            public string score;
+
+            public patterndata(string number, string name, string views, string sells, string wishlists, string ratings, string score)
+            {
+                this.number = number;
+                this.name = name;
+                this.views = views;
+                this.sells = sells;
+                this.wishlists = wishlists;
+                this.ratings = ratings;
+                this.score = score;
+            }
+            public string toString()
+            {
+                return "";
+            }
+            public List<String> getData()
+            {
+                var retlist = new List<string>();
+                retlist.Add(this.number);
+                retlist.Add(this.name);
+                retlist.Add(this.views);
+                retlist.Add(this.sells);
+                retlist.Add(this.wishlists);
+                retlist.Add(this.ratings);
+                retlist.Add(this.score);
+                
+                return retlist;
+            }
+        }
 
 
         public Form1()
@@ -29,7 +68,7 @@ namespace httprequesttests
 
         public class CookieAwareWebClient : WebClient
         {
-            private CookieContainer cookie = new CookieContainer();
+            public CookieContainer cookie = new CookieContainer();
 
             protected override WebRequest GetWebRequest(Uri address)
             {
@@ -40,74 +79,30 @@ namespace httprequesttests
                 }
                 return request;
             }
+
         }
 
-        private void button_request_Click(object sender, EventArgs e)
-        {
-            WebRequest request = WebRequest.Create(myURL);
-            request.Headers.Add("Cookie", myCookie);
-
-            WebResponse response = request.GetResponse();
-            Stream data = response.GetResponseStream();
-            string html = String.Empty;
-            using (StreamReader sr = new StreamReader(data))
-            {
-                html = sr.ReadToEnd();
-                textBox_response.Text = html;
-            }
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            formUrl = textBox_LoginURL.Text; // NOTE: This is the URL the form POSTs to, not the URL of the form (you can find this in the "action" attribute of the HTML's form tag
-            myURL = textBox_url.Text;
-            myBaseURL = textBox_BaseURL.Text;
             myPW = ConfigurationManager.AppSettings["myPW"];
             myUsername = ConfigurationManager.AppSettings["myUsername"];
-            myCookie = ConfigurationManager.AppSettings["myCookie"];
-            //MessageBox.Show(myCookie);
-
-
-
-
 
         }
 
-        private void button_Login_Click(object sender, EventArgs e)
-        {
-
-            string formParams = string.Format("username={0}&password={1}&permalogin={2}", myUsername, myPW, "1");
-            WebRequest req = WebRequest.Create(formUrl);
-            req.ContentType = "application/x-www-form-urlencoded";
-            req.Method = "POST";
-            byte[] bytes = Encoding.ASCII.GetBytes(formParams);
-            req.ContentLength = bytes.Length;
-            using (Stream os = req.GetRequestStream())
-            {
-                os.Write(bytes, 0, bytes.Length);
-            }
-            WebResponse resp = req.GetResponse();
-            cookieHeader = resp.Headers["Set-cookie"];
-
-            //MessageBox.Show(cookieHeader);
-            myCookie = cookieHeader;
-
-
-        }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
 
 
             Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
-            config.AppSettings.Settings.Remove("myCookie");
-            config.AppSettings.Settings.Add("myCookie", myCookie);
+            //config.AppSettings.Settings.Remove("myCookie");
+            //config.AppSettings.Settings.Add("myCookie", myCookie);
             config.Save(ConfigurationSaveMode.Minimal);
 
-                                   
         }
 
-        private void button_WebClient_Login_Click(object sender, EventArgs e)
+        private bool login()
         {
             client = new CookieAwareWebClient();
             client.Encoding = Encoding.UTF8;
@@ -116,12 +111,38 @@ namespace httprequesttests
             loginData.Add("username", myUsername);
             loginData.Add("password", myPW);
             loginData.Add("permalogin", "1");
-            client.UploadValues("users/login", "POST", loginData);
+            try
+            {
+                client.UploadValues(myLoginPage, "POST", loginData);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return getClientLoggedin();
+
+        }
+
+        private bool getClientLoggedin()
+        {
+            
+            return false;
+            //            if (client.cookie.ToString) { }
+
         }
 
         private void button_Webclient_Request_Click(object sender, EventArgs e)
         {
-            string page =  client.DownloadString("patman");
+            if (!getClientLoggedin())
+            {
+                login();
+            }
+
+
+            string page = client.DownloadString(myPatmanPage);
             textBox_response.Text = page;
 
             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
@@ -132,26 +153,54 @@ namespace httprequesttests
             .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
             .ToList();
 
-            foreach(List<string> Rows in TDs)
+
+
+            //fill newList
+            foreach (List<string> Rows in TDs)
             {
+                patterndata pat = new patterndata(
+                    Rows[3],  //number
+                    Rows[2].Substring(0, Rows[2].IndexOf("\r\n")),  //name
+                    Rows[8],  //views
+                    Rows[9],  //sells
+                    Rows[13], //wishlists
+                    Rows[15], //ratings
+                    Rows[14]  //score
+                    );
 
-                string name = Rows[2];
-                string number = Rows[3];
-                string views = Rows[8];
-                string sells = Rows[9];
-                string wishlists = Rows[13];
-                string ratings = Rows[15];
-
-                name = name.Substring(0, name.IndexOf("\r\n"));
-                               
-                DG1.Rows.Add(name,number,views,sells,wishlists,ratings);
-                                
-
+                newList.Add(pat);
+                
             }
 
+            //populate DataGridView...
+            DG1.Rows.Clear();
+            foreach (patterndata pat in newList)
+            {
+                DG1.Rows.Add(
+                    pat.number,
+                    pat.name,
+                    pat.views,
+                    pat.sells,
+                    pat.wishlists,
+                    pat.ratings,
+                    pat.score);
+            }
+            
+        }
 
-
+        private void button_Timer_Toggle_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                button_Timer_Toggle.Text = "Activate Tracking";
+            }
+            else
+            {
+                timer1.Enabled = true;
+                button_Timer_Toggle.Text = "Stop Tracking";
+            }
 
         }
     }
-}
+}           
